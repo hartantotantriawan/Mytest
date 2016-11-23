20a33a26a569b1e00004eb152a1e9ee91b8721d6
hartantotantriawan <tantozayl79@gmail.com>
2016-11-23 17:29:44 +0700

    some_message

---
+++ calc.py
@@ -0,0 +1,6 @@
+
+while True:
+  hitung = raw_input('Hitung ')
+  if not hitung:
+   break
+  print eval(hitung)
---
+++ list.py
@@ -0,0 +1,13 @@
+
+#for ch in 'Bummi':
+#   print ch,
+#
+
+
+data = ['Jono','tinggal','bersama','Joni','di kopi 3']
+
+print data[0]; print data[1]
+print data[2]
+print data[3]
+
+
